42449d0fe2f504276a6693a42a681cb31e8c1877
Adeline Gayle Noman <adeline.gayle.noman@gmail.com>
2022-06-25 05:00:00 +0800

    add google oauth2 and project setup

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+require('./services/passport');
+
 const app = express();
 
-app.get('/',  (req,res) => {
-    res.send({hi: 'there'});
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> {
-    console.log(`App listening to port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`App listening to port ${PORT}`);
+});
